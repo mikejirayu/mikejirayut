baacd7bd209884df32a51f3e19c48331960b11b3
mikejirayu <36217306+mikejirayu@users.noreply.github.com>
2018-02-07 11:19:50 +0700

    Update mybot.php

--- mybot.php
+++ mybot.php
@@ -25,7 +25,7 @@ if (!is_null($events['events'])) {
 				// Build message to reply back
 				$messages = [
 					'type' => 'text',
-					'text' => 'เอ้อ เชื่อฟังบอกง่าย <3 '
+					'text' => 'เอ้อ เชื่อฟังบอกง่าย ดีมากๆ 555 '
 				];
 			}
 		
